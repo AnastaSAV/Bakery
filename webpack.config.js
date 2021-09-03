a41e8ea9e41e77c84c2bc91173e2a946dbeb9f16
AnastaSAV <anastasia.personalbox@gmail.com>
2021-09-03 15:56:19 +0300

    added fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 let mode = "development";
 let target = "web";
@@ -13,19 +15,31 @@ module.exports = {
 	target: target,
 	
 	output: {
-		assetModuleFilename: "images/[hash][ext][query]"
+		path: path.resolve(__dirname, "dist"),
+		assetModuleFilename: "images/[hash][ext][query]",
 	},
 	
 	module: {
 		rules: [
 			{
+				test: /\.html$/,
+				use: 'html-loader' // important to use, images in HTML dont work without this loader
+			},
+			{
 				test: /\.(png|jpe?g|gif|svg)$/i,
-				type: "asset", // use /inline if dont work
-				// parser: {
-				// 	dataUrlCondition: {
-				// 		maxSize: 30 * 1024,
-				// 	},
-				// },
+				type: "asset/resource", // use /inline if dont work
+				parser: {
+					dataUrlCondition: {
+						limit: 30 * 1024,
+					},
+				},
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+				generator: {
+					         filename: 'fonts/[hash][ext][query]'
+				    },
 			},
 			{
 				test: /\.(s[ac]|c)ss$/i,
@@ -49,17 +63,22 @@ module.exports = {
 		],
 	},
 	plugins: [
-		new MiniCssExtractPlugin()
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin(),
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+		}),
 	],
-	
 	
 	devtool: "source-map",
 	devServer: {
+		historyApiFallback: true,
 		static: {
-			directory: path.join(__dirname, 'dist'),
+			directory: path.resolve(__dirname, './dist'),
 		},
+		open: true,
 		compress: true,
-		port: 8080,
 		hot: true,
+		port: 9000,
 	},
 };
