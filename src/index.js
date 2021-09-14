05be401c762b8b9920f3c1ed2b4523670fd4912b
AnastaSAV <anastasia.personalbox@gmail.com>
2021-09-14 14:13:48 +0300

    added section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import "./styles/index.scss"
+import "./styles/index.scss";
+import "./fonts/Alegreya-Bold.ttf";
+import "./fonts/Alegreya-Regular.ttf";
+import "./fonts/DancingScript-Regular.ttf";
+import "./fonts/PlayfairDisplay-Bold.ttf";
+import "./fonts/Roboto-Regular.ttf";
 
 const elvenShieldRecipe = {
 	leatherStrips: 2,
