1addf69a2c6905b3400a7e46ecc5a66dcf739838
AnastaSAV <anastasia.personalbox@gmail.com>
2021-09-30 09:47:36 +0300

    slider added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./sliders.js";
 import "./styles/index.scss";
 import "./fonts/Alegreya-Bold.ttf";
 import "./fonts/Alegreya-Regular.ttf";
@@ -5,9 +6,11 @@ import "./fonts/DancingScript-Regular.ttf";
 import "./fonts/PlayfairDisplay-Bold.ttf";
 import "./fonts/Roboto-Regular.ttf";
 
-const elvenShieldRecipe = {
-	leatherStrips: 2,
-	ironIngot: 1,
-	refineMoonstone: 4
-};
-console.log(elvenShieldRecipe);
+
+// const elvenShieldRecipe = {
+// 	leatherStrips: 2,
+// 	ironIngot: 1,
+// 	refineMoonstone: 4
+// };
+// console.log(elvenShieldRecipe);
+
